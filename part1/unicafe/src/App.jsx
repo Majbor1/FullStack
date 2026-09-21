8cceb4b79eb129e15ea08ff4c62f45ce0f53907b
Marcin Majborski <marcinmajborski@gmail.com>
2026-09-21 13:58:32 +0200

    p1ex8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
 
-const Display = (props) => <h1>{props.text}</h1>
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
-const Stats = (props) => <p>{props.text} {props.name}</p>
+
+const Stats = (props) => {
+  const total = props.good + props.neutral + props.bad
+  const average = () => (props.good - props.bad)/total
+  const positive = () => {
+    const positive = (props.good/total)*100
+    return (positive + '%')
+  }
+  
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>
+        good {props.good} <br />
+        neutral {props.neutral} <br />
+        bad {props.bad} <br />
+        all {props.total} <br />
+        average {average()} <br />
+        positive {positive()} <br />
+      </p>
+    </div>
+  )
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  
+  
 
   const handleGoodClick = () => {
     console.log('good clicked')
@@ -32,25 +55,14 @@ const App = () => {
     setTotal(good + neutral + updatedBad)
   }
   
-  const average = () => (good - bad)/total
-  const positive = () => {
-    const positive = (good/total)*100
-    return (positive + '%')
-  }
-
   return (
     <div>
-      <Display text='give feedback' />
+      <h1>give feedback</h1>
       <Button onClick={handleGoodClick} text='good' />
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
-      <Display text='statistics' />
-      <Stats text='good' name={good} />
-      <Stats text='neutral' name={neutral} />
-      <Stats text='bad' name={bad} />
-      <Stats text='all' name={total} />
-      <Stats text='average' name={average()} />
-      <Stats text='positive' name={positive()} />
+      <Stats good={good} neutral={neutral} bad={bad}/>
+      
     </div>
   )
 }
